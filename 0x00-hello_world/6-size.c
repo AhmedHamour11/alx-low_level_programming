e557e995a7ef98a32dfc85ad081c305f8a627831
AhmedHamour11 <ahmedhamour9339@gmail.com>
2023-03-16 18:23:59 -0700

    fdhfhjf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * * main - a program that the size of various types on the computer
+ * * Return: 0 (This program will be successful)
+ * */
+int main(void)
+{
+char a;
+int b;
+long int lo-i;
+long long int lo-lo;
+float fl;
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
+printf("Size of an int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", sizeof(lo-i));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(lo-lo));
+printf("Size of a float: %lu byte(s)\n", sizeof(fl));
+return (0);
+}
