6c7dea70ebbef9817da10ebfaab7efb009dbf136
AhmedHamour11 <ahmedhamour9339@gmail.com>
2023-03-17 17:06:04 -0700

    vgg

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - functin
- * Description:  program that prints the alphabet in lowercase, followed by a new line.
+ * Description:  program that prints the alphabet in lowercase,
+ * followed by a new line.
  * Return: 0
  */
 
