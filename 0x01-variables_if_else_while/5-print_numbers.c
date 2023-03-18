f86e921e231314a1f3ecffcd9b63a655ea1af211
AhmedHamour11 <ahmedhamour9339@gmail.com>
2023-03-17 17:19:02 -0700

    fff

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+i#include <stdio.h>
+/**
+ * main - functin
+ * Description:  program that prints the alphabet in lowercase,
+ * followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
