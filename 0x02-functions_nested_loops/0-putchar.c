81da783c21aee98c2bab1625620d1fb9bf8d31ce
AhmedHamour11 <ahmedhamour9339@gmail.com>
2023-03-21 15:23:56 -0700

    fdkfd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
-/**
- * _putchar - writes the character c to stdout 
- * @c: the character to print 
- *
- * Return: On success 1.
- * 
- */
-
 int main
 {	printf("_putchar\n");
-        return 0;
+	return 0;
 }
