908e2c27fab039b5ab4ea67915a619147f9d2a6b
AhmedHamour11 <ahmedhamour9339@gmail.com>
2023-03-21 16:32:24 -0700

    sd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_alphabet() {
+	char letter;
+		for (letter = 'a'; letter <= 'z'; letter++) {
+		 printf("%c\n", letter);				    }
+}
+	int main() {
+		print_alphabet();
+	        return 0;
+}
+
