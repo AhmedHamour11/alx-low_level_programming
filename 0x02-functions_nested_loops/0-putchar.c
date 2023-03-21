bb36e01375af914c9e40203fb9f504c34b10abc9
AhmedHamour11 <ahmedhamour9339@gmail.com>
2023-03-21 16:07:10 -0700

    dffd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,18 @@
 #include "main.h"
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
 int main(void)
-{	printf("_putchar\n");
-	return 0;
+{
+	char *ch = "_putchar\n";
+	int i = 0;
+
+	while (ch[i])
+	{
+			_putchar(ch[i]);
+			i++;
+	}
+	return (0);
 }
