8df7799f4a888b24a464077379aef66d3ec24686
AhmedHamour11 <ahmedhamour9339@gmail.com>
2023-03-21 16:49:19 -0700

    sd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *print_alphabet - print all char from a to z
-* Description: 'the program's description'
+* Description:'the program's description'
 */
 void print_alphabet(void)
 {
